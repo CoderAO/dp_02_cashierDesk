f15e146d02a8564f5b76e4fd2b9dbe37f942cc3e
aoran <32862082@qq.com>
2015-04-30 09:57:16 +0800

    工厂和策略结合

--- CashierDesk/CashContext.m
+++ CashierDesk/CashContext.m
@@ -8,7 +8,13 @@
 
 #import "CashContext.h"
 #import "CashSuper.h"
-#import "CashFactory.h"
+#import "Plan.h"
+#import "PlanNormal.h"
+#import "PlanDiscount.h"
+#import "PlanReturn.h"
+#import "CashNormal.h"
+#import "CashDiscount.h"
+#import "CashReturn.h"
 
 @interface CashContext()
 
@@ -25,8 +31,23 @@
 
 - (float)getResult:(float)shouldPrice
 {
-    CashSuper *csuper = [CashFactory cashSuperWithPlan:self.plan];
+    CashSuper *csuper =  [self cashSuperWithPlan:self.plan];
     return [csuper acceptCash:shouldPrice];
 }
 
+// 根据传入plan的不同类型来确定初始化哪种计算的类
+- (CashSuper *)cashSuperWithPlan:(Plan *)plan
+{
+    CashSuper *csuper = nil;
+    if ([plan isKindOfClass:[PlanNormal class]]) {
+        csuper = [CashNormal cashWithPlan:nil];
+    }
+    else if ([plan isKindOfClass:[PlanDiscount class]]){
+        csuper = [CashDiscount cashWithPlan:plan];
+    }
+    else if ([plan isKindOfClass:[PlanReturn class]]){
+        csuper = [CashReturn cashWithPlan:plan];
+    }
+    return csuper;
+}
 @end
